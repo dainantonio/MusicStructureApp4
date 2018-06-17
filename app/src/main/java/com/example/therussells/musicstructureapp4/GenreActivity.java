ac0ffd2645685944234029fee631c3d7c23a48a6
TheRussells <dain.russell@gmail.com>
2018-06-17 00:29:56 -0400

    //variable to keep track of the current index position         int index = 0;
            // Will keep looping until the end of the list at index 9. as long as the index is less than the length of the list
            while (index < songs.size()) {
                TextView songlistView = new TextView(this);
    
                // Set the text to be song at the current index
                songlistView.setText(songs.get(index));
    
                // Add this TextView as another child to the genre view of this layout
                genreView.addView(songlistView);
    
                // Increment the index variable by 1
                index++;

--- app/src/main/java/com/example/therussells/musicstructureapp4/GenreActivity.java
+++ app/src/main/java/com/example/therussells/musicstructureapp4/GenreActivity.java
@@ -28,23 +28,25 @@ public class GenreActivity extends AppCompatActivity {
         songs.add("spanish town rockin' ");
         songs.add("tell me");
 
-        // Verify the contents of the array by printing out each array element to the logs
-        Log.v("GenreActivity", "song at index 0: " + songs.get(0));
-        Log.v("GenreActivity", "song at index 1: " + songs.get(1));
-        Log.v("GenreActivity", "song at index 2: " + songs.get(2));
-        Log.v("GenreActivity", "song at index 3: " + songs.get(3));
-        Log.v("GenreActivity", "song at index 4: " + songs.get(4));
-        Log.v("GenreActivity", "song at index 5: " + songs.get(5));
-        Log.v("GenreActivity", "song at index 6: " + songs.get(6));
-        Log.v("GenreActivity", "song at index 7: " + songs.get(7));
-        Log.v("GenreActivity", "song at index 8: " + songs.get(8));
-        Log.v("GenreActivity", "song at index 9: " + songs.get(9));
-        Log.v("GenreActivity", "song at index 10: " + songs.get(10));
-
+        // Find the genre view so we can add child views (list of songs) to it
         LinearLayout genreView = findViewById(R.id.genreView);
-        TextView songlistView = new TextView(this);
-        songlistView.setText(songs.get (0));
-        genreView.addView(songlistView);
+
+        // variable to keep track of the current index position
+        int index = 0;
+
+        // Will keep looping until the end of the list at index 9. as long as the index is less than the leght of the list
+        while (index < songs.size()) {
+            TextView songlistView = new TextView(this);
+
+            // Set the text to be song at the current index
+            songlistView.setText(songs.get(index));
+
+            // Add this TextView as another child to the genre view of this layout
+            genreView.addView(songlistView);
+
+            // Increment the index variable by 1
+            index++;
+        }
 
     }
 }
