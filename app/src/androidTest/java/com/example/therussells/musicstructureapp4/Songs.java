37253cf94e26d4e7693de721c3e17afbf28386f8
TheRussells <dain.russell@gmail.com>
2018-06-18 22:53:58 -0400

    add listview and custom class to get song and artist name

---
+++ app/src/androidTest/java/com/example/therussells/musicstructureapp4/Songs.java
@@ -0,0 +1,27 @@
+package com.example.therussells.musicstructureapp4;
+
+public class Songs {
+
+    //Song Names
+    private String mSongsName;
+
+    //Artist Name
+    private String mArtistName;
+
+    //new Songs Object created
+    public Songs(String mSongsName, String mArtistName) {
+       mSongsName = SongsName;
+        mArtistName = ArtistName;
+    }
+
+    ///get song name
+    public String getSongsName() {
+        return mSongsName;
+    }
+
+    // get artist name
+    public String getArtistName() {
+        return mArtistName;
+    }
+
+}
